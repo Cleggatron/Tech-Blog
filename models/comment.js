53674a96275100e6099c9cb33eccb4684c5fd4b1
Cleggatron <david.robert.clegg@gmail.com>
2021-09-06 14:58:44 +0100

    added comment model and imported to index

---
+++ models/comment.js
@@ -0,0 +1,42 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection")
+
+class Comment extends Model{
+
+}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: "post",
+                key: "id"
+            }
+        }
+    },
+    {
+        //passes the connection
+        sequelize,
+        //handle our created at
+        timestamps: true,
+        updatedAt: false,
+        freezeTableName: true,
+        underscored: true,
+        //table name
+        modelName: "comment"
+    }
+)
+
+
+module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -1,2 +1,6 @@
 const User = require("./user");
 const Post = require("./post");
+const Comment = require("./comment");
+
+
+module.exports = {User, Post, Comment}
