2b9bfc170530264f710c8700204391329158427e
Cleggatron <david.robert.clegg@gmail.com>
2021-09-06 17:53:51 +0100

    set up handlebars and middlewares

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
+//npm dependencies
+const path = require("path")
 const express = require("express");
+const exphbs = require("express-handlebars")
+//file dependencies
 const routers = require("./controllers");
-
-const PORT = 3001;
 const sequelize = require("./config/connection")
+
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//set up handlebars
+const hbs = exphbs.create()
+app.engine("handlebars", hbs.engine)
+app.set("view engine", "handlebars")
 
 
 
 
 
-
-
+//standard middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+//set up static assets 
+//TODO: Build static assets
+app.use(express.static(path.join(__dirname, "public")));
 
 
 //set up our routing
