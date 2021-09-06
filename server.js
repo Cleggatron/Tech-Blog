12fb2c1575ba49ce57293a3a5360798d332f9073
Cleggatron <david.robert.clegg@gmail.com>
2021-09-06 17:20:04 +0100

    set up basic routing

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const routers = require("./controllers");
 
 const PORT = 3001;
 const sequelize = require("./config/connection")
 
 const app = express();
+
+
+
+
+
+
+
+
+
+
+//set up our routing
+app.use(routers)
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
