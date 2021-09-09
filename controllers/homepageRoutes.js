2d8372f65c57b0f1b5dec9ac5a445edb4d459e37
Cleggatron <david.robert.clegg@gmail.com>
2021-09-09 11:43:29 +0100

    renamed file for semantic reasons

---
+++ controllers/homepageRoutes.js
@@ -0,0 +1,33 @@
+const {User, Post} = require("../models");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+    try{
+        //get our raw data
+        const homepageData = await Post.findAll(
+            {
+                attributes: ["title", "createdAt"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            }
+        ).catch((err) => {
+            res.json(err);
+        })
+        
+        //trim back the data to a manageable format
+        const posts = homepageData.map(post => post.get({plain: true}));
+        
+        //render the page using handlebars
+        res.render("homepage", {
+            posts
+        })
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
