8c4647b301080c2ebe8f2068d3c0b476b5d704ed
Cleggatron <david.robert.clegg@gmail.com>
2021-09-12 10:19:16 +0100

    added routing for the login page

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,12 @@
 const router =  require("express").Router();
 const homeRoutes = require("./homepageRoutes");
 const dashboardRoutes = require("./dashboardRoutes")
+const loginPageRoutes = require("./loginPageRoutes")
 const apiRoutes = require("./api");
 
 router.use("/", homeRoutes);
 router.use("/dashboard", dashboardRoutes);
 router.use("/api", apiRoutes);
+router.use("/login", loginPageRoutes);
 
 module.exports = router;
